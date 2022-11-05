f4fa35265c094706b3040964280e0d0c8de0bd46
Steve Hill <sghill.dev@gmail.com>
2022-11-05 16:48:21 -0700

    Correct for hpi:hpi not producing the src/main/resources/index.jelly file

--- src/main/java/net/sghill/flexibuild/discover/Maven.java
+++ src/main/java/net/sghill/flexibuild/discover/Maven.java
@@ -4,6 +4,9 @@ import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.io.IoBuilder;
 
 import java.io.OutputStream;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
 
 public class Maven implements BuildTool {
     @Override
@@ -18,7 +21,7 @@ public class Maven implements BuildTool {
 
     @Override
     public String pkg() {
-        return mvn("hpi:hpi");
+        return mvn("package", "hpi:hpi");
     }
 
     @Override
@@ -40,7 +43,13 @@ public class Maven implements BuildTool {
                 .buildOutputStream();
     }
 
-    private static String mvn(String arg) {
-        return String.join(" ", "mvn", "--batch-mode", "--no-transfer-progress", arg);
+    private static String mvn(String arg, String... rest) {
+        List<String> args = new LinkedList<>();
+        args.add("mvn");
+        args.add("--batch-mode");
+        args.add("--no-transfer-progress");
+        args.add(arg);
+        args.addAll(Arrays.asList(rest));
+        return String.join(" ", args);
     }
 }
